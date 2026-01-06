e45a0e775f6175b6aa238d8e4a14b86595c23b43
Santiago Sepúlveda <santiagosepul2109@gmail.com>
2026-01-06 21:01:43 +0100

    Portadas corregidas, botones acceso rapido

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import ModalImagenes from "@/components/ModalImagenes";
 import BibliotecaSection from "@/components/BibliotecaSection";
 import HeroSection from "@/components/HeroSection";
 import NuevoModelo from "@/components/NuevoModelo";
+import LinksSection from "@/components/LinksSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <LinksSection />
       <BibliotecaSection />
       <NuevoModelo />
       <ModalImagenes />
